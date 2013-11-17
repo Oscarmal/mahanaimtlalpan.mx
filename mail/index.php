e60d02d96daf6ee41cb2bea3e1295f6b33958bce
Oscarmal <oscarmaldonado_1@hotmail.com>
2013-11-16 19:33:56 -0600

    Segundo commit

---
+++ mail/index.php
@@ -0,0 +1,5 @@
+<?php 
+header('Location: http://mail.google.com/a/mahanaimtlalpan.mx');
+#header('Location: http://webmail.000webhost.com/roundcube/');
+#http://drive.google.com/a/mahanaimtlalpan.mx
+?>
